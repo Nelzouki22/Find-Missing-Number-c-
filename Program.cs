ad6f3d56ca7bec200d2aca107a1d2a4e6072e721
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-07-22 20:37:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static int FindMissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        int total = (n + 1) * (n + 2) / 2; // Sum of first n+1 natural numbers
+
+        for (int i = 0; i < n; i++)
+        {
+            total -= nums[i];
+        }
+
+        return total;
+    }
+
+    static void Main()
+    {
+        int[] nums = { 1, 2, 4, 5, 6 }; // مثال على مصفوفة بها رقم مفقود
+        int missingNumber = FindMissingNumber(nums);
+
+        Console.WriteLine("The missing number is: " + missingNumber);
+    }
+}
